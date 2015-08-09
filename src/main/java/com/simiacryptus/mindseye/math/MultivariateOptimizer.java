aec8172913dc63a33ce89a4abb432333f1304009
Andrew Charneski <acharneski@gmail.com>
2015-08-08 20:45:06 -0700

    wip

--- src/main/java/com/simiacryptus/mindseye/math/MultivariateOptimizer.java
+++ src/main/java/com/simiacryptus/mindseye/math/MultivariateOptimizer.java
@@ -1,12 +1,20 @@
 package com.simiacryptus.mindseye.math;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import org.apache.commons.math3.analysis.MultivariateFunction;
 import org.apache.commons.math3.optim.PointValuePair;
+import org.apache.commons.math3.util.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Lists;
 
 public class MultivariateOptimizer {
   static final Logger log = LoggerFactory.getLogger(MultivariateOptimizer.class);
@@ -39,12 +47,35 @@ public class MultivariateOptimizer {
   }
 
   public double[] step(final double[] start) {
+    //Set<Integer> toMutate = chooseIndexes(1, start.length);
+    ArrayList<Integer> l = new ArrayList<>(IntStream.range(0, start.length).mapToObj(x->x).collect(Collectors.toList()));
+    Collections.shuffle(l);
+    Pair<Integer, double[]> opt = l.stream().map(i->{
+      try {
+        return new Pair<>(i,step(start, new HashSet<Integer>(Arrays.asList(i))));
+      } catch (Exception e) {
+        log.debug("Error mutating " + i, e);
+        return null;
+      }
+    }).filter(x->null!=x).findFirst().get();
+    double[] next = Arrays.copyOf(start, start.length);
+    next[opt.getFirst()] = opt.getSecond()[opt.getFirst()];
+    return next;
+    //return step(start, l.stream().collect(Collectors.toSet()));
+  }
+
+  public Set<Integer> chooseIndexes(int numberOfSelections, int maxValue) {
+    Set<Integer> toMutate = IntStream.generate(()->(int)(maxValue*Math.random())).distinct().limit(numberOfSelections).mapToObj(x->x).collect(Collectors.toSet());
+    return toMutate;
+  }
+
+  public double[] step(final double[] start, Set<Integer> toMutate) {
     return IntStream.range(0, start.length).parallel().mapToDouble(i->{
-      return new UnivariateOptimizer(x->{
+      return toMutate.contains(i)?new UnivariateOptimizer(x->{
         double[] pt = Arrays.copyOf(start, start.length);
         pt[i] = x;
         return f.value(pt);
-      }).minimize(start[i]).getFirst()[0];
+      }).minimize(start[i]).getFirst()[0]:start[i];
     }).toArray();
   }
 
