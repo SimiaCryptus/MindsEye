07b29dd763fe18ff7f93bc21a0342bf5b8da6d6f
Andrew Charneski <acharneski@gmail.com>
2017-05-21 16:21:55 -0700

    Added standardized (java) SparkTrainable class

--- src/main/java/com/simiacryptus/mindseye/opt/SparkTrainable.java
+++ src/main/java/com/simiacryptus/mindseye/opt/SparkTrainable.java
@@ -21,39 +21,130 @@ package com.simiacryptus.mindseye.opt;
 
 import com.simiacryptus.mindseye.graph.dag.DAGNetwork;
 import com.simiacryptus.mindseye.net.DeltaSet;
+import com.simiacryptus.mindseye.net.NNLayer;
 import com.simiacryptus.mindseye.net.NNResult;
 import com.simiacryptus.util.ml.Tensor;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.rdd.RDD;
 
-import java.util.Arrays;
+import java.io.Serializable;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public class SparkTrainable implements Trainable {
   
-  private final Tensor[][] trainingData;
+  private static class ReducableResult implements Serializable {
+    public final Map<String, double[]> deltas;
+    public final double sum;
+    public final int count;
+    
+    public ReducableResult(Map<String, double[]> deltas, double sum, int count) {
+      this.deltas = deltas;
+      this.sum = sum;
+      this.count = count;
+    }
+    
+    public void accumulate(DeltaSet source) {
+      Map<String, NNLayer> idIndex = source.map.entrySet().stream().collect(Collectors.toMap(
+          e -> e.getKey().id.toString(), e -> e.getKey()
+      ));
+      deltas.forEach((k,v)->source.get(idIndex.get(k), (double[])null).accumulate(v));
+    }
+    
+    public ReducableResult add(ReducableResult right) {
+      HashMap<String, double[]> map = new HashMap<>();
+      Set<String> keys = Stream.concat(deltas.keySet().stream(), right.deltas.keySet().stream()).collect(Collectors.toSet());
+      for(String key : keys) {
+        double[] l = deltas.get(key);
+        double[] r = right.deltas.get(key);
+        if(null != r) {
+          if(null != l) {
+            assert(l.length==r.length);
+            double[] x = new double[l.length];
+            for(int i=0;i<l.length;i++) x[i] = l[i]+r[i];
+            map.put(key, x);
+          } else {
+            map.put(key, r);
+          }
+        } else {
+          assert(null != l);
+          map.put(key, l);
+        }
+      }
+      return new ReducableResult(map, sum+right.sum, count+right.count);
+    }
+    
+    public double meanValue() {
+      return sum / count;
+    }
+  }
+  
+  private static class PartitionTask implements FlatMapFunction<Iterator<Tensor[]>, ReducableResult> {
+    final DAGNetwork network;
+    
+    private PartitionTask(DAGNetwork network) {
+      this.network = network;
+    }
+    
+    @Override
+    public Iterator<ReducableResult> call(Iterator<Tensor[]> partition) throws Exception {
+      Tensor[][] tensors = SparkTrainable.getStream(partition).toArray(i -> new Tensor[i][]);
+      NNResult eval = network.eval(NNResult.batchResultArray(tensors));
+      DeltaSet deltaSet = new DeltaSet();
+      eval.accumulate(deltaSet);
+      double[] doubles = Arrays.stream(eval.data).mapToDouble(x -> x.getData()[0]).toArray();
+      return Arrays.asList(SparkTrainable.getResult(deltaSet, doubles)).iterator();
+    }
+  }
+  private static ReducableResult getResult(DeltaSet delta, double[] values) {
+    Map<String, double[]> deltas = delta.map.entrySet().stream().collect(Collectors.toMap(
+        e -> e.getKey().id.toString(), e -> e.getValue().delta
+    ));
+    return new ReducableResult(deltas, Arrays.stream(values).sum(), values.length);
+  }
+  
+  private DeltaSet getDelta(ReducableResult reduce) {
+    DeltaSet deltaSet = new DeltaSet();
+    Tensor[] prototype = dataRDD.take(1).get(0);
+    NNResult result = network.eval(NNResult.batchResultArray(new Tensor[][]{prototype}));
+    result.accumulate(deltaSet, 0);
+    reduce.accumulate(deltaSet);
+    return deltaSet;
+  }
+  
+  private final JavaRDD<Tensor[]> dataRDD;
   private final DAGNetwork network;
   
-  public SparkTrainable(Tensor[][] trainingData, DAGNetwork network) {
-    this.trainingData = trainingData;
+  public SparkTrainable(RDD<Tensor[]> trainingData, DAGNetwork network) {
+    this.dataRDD = trainingData.toJavaRDD();
     this.network = network;
     resetSampling();
   }
   
   @Override
   public PointSample measure() {
-    NNResult[] input = NNResult.batchResultArray(trainingData);
-    NNResult result = network.eval(input);
-    DeltaSet deltaSet = new DeltaSet();
-    result.accumulate(deltaSet);
+    ReducableResult result = dataRDD.mapPartitions(new PartitionTask(network))
+                                 .reduce(ReducableResult::add);
+    DeltaSet deltaSet = getDelta(result);
     DeltaSet stateSet = new DeltaSet();
     deltaSet.map.forEach((layer, layerDelta) -> {
       stateSet.get(layer, layerDelta.target).accumulate(layerDelta.target);
     });
-    assert (Arrays.stream(result.data).allMatch(x -> x.dim() == 1));
-    double meanValue = Arrays.stream(result.data).mapToDouble(x -> x.getData()[0]).average().getAsDouble();
-    return new PointSample(deltaSet, stateSet, meanValue);
+    return new PointSample(deltaSet, stateSet, result.meanValue());
   }
   
   @Override
   public void resetToFull() {
+  }
+  
+  private static Stream<Tensor[]> getStream(Iterator<Tensor[]> partition) {
+    int characteristics = Spliterator.ORDERED;
+    boolean parallel = false;
+    Spliterator<Tensor[]> spliterator = Spliterators.spliteratorUnknownSize(partition, characteristics);
+    return StreamSupport.stream(spliterator, parallel);
   }
   
   @Override
